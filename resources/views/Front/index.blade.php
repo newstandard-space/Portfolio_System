19fd4c32df1b0c769944e88a38a4f927407ceaf9
go-to-space <[newstandardrise@gmail.com]>
2023-03-03 14:46:25 +0900

    ホーム画面 empty条件追加

--- resources/views/Front/index.blade.php
+++ resources/views/Front/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 
-
+@if(!empty($about_detail))
 <section id="about">
     <h1>About Me</h1>
     <div class="work">
@@ -13,6 +13,8 @@
     </div>
     <p class="text-center text-muted"><a href="{{ route('front.about.index') }}" class="default-a-tag">詳しく見る</a></p>
 </section>
+@endif
+@if(!empty($work))
 <section id="works">
     <h1>My Works</h1>
     <div class="work">
@@ -23,7 +25,6 @@
     </div>
     <p class="text-center text-muted"><a href="{{ route('front.work.index') }}" class="default-a-tag">詳しく見る</a></p>
 </section>
-
-
+@endif
 
 @endsection
